e1fa9f77fad15690145e512a35cf1e745382319a
domotace <eytan.domota--condo@edu.esiee.fr>
2025-09-22 20:56:56 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 #### Fonction secondaire
-
-
+import unicodedata
+import re
 def ispalindrome(p):
-
-    # votre code ici
+#on convertit les majuscules en minuscules    
+    p = p.lower()
+#on se débarasse des accents
+    p = unicodedata.normalize('NFD', p)
+    p = ''.join(char for char in p if unicodedata.category(char) != 'Mn')
+#on garde seulement les lettres et chiffres
+    p = re.sub(r'[^a-zA-Z0-9]', '', p)
+#on veut comparer la CDC avec sa version inversée
+    return p == p[::-1]
     
-    return False
 
 #### Fonction principale
 
